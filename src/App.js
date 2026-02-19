a696396cb4bb3a0faf17c1f53d6211ecb95d68bf
ecrow90 <eric.school.gbc@gmail.com>
2026-02-19 15:24:06 -0500

    Updated app,js file

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Studen ID: Put Your Student ID</h1>
-      <h1>Studen ID: Put Your Student Name</h1>
+      <h1>Student ID: 101584030</h1>
+      <h1>Student Name: Eric Crowley</h1>
       <h1>Student Information System</h1>
       <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
       <StudentForm addStudent={addStudent} />
